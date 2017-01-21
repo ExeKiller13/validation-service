9c5a8fe9f7f45ec684c1d0ab856ade660176df99
ExeKiller13 <exekiller13@gmail.com>
2017-01-21 17:02:59 +0200

    Refactored code

--- src/main/java/alokhin/service/validators/PasswordValidator.java
+++ src/main/java/alokhin/service/validators/PasswordValidator.java
@@ -6,30 +6,36 @@ public class PasswordValidator extends Validator {
         super();
     }
 
+    private static final String PASSWORD_PATTERN_A = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$";
+    private static final String PASSWORD_PATTERN_B = "^[\u0000-\u007F]*$";
+    private static final String PASSWORD_PATTERN_C = "(?:abcd|dcba|bcde|edcb|cdef|fedc|defg|gfed|efgh|hgfe|fghi|ihgf|ghij|jihg|hijk|kjih|ijkl|lkji|jklm|mlkj|klmn|nmlk|lmno|onml|mnop|ponm|nopq|qpon|opqr|rqpo|pqrs|srqp|qrst|tsrq|rstu|utsr|stuv|vuts|tuvw|wvut|uvwx|xwvu|vwxy|yxwv|wxyz|zyxw|0123|1234|2345|3456|4567|5678|6789)";
+    private static final String PASSWORD_PATTERN_D = "(\\w+)\\1{3,}";
+
     @Override
     public boolean validate(String value) {
 
-        boolean firstCondition = value.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$"); // Regular Expression showing the password satisfies the condition A
-        if(!firstCondition) {
+        boolean firstCondition = value.matches(PASSWORD_PATTERN_A); // Regular Expression showing the password satisfies the condition A
+        if (!firstCondition) {
             getState().append("Failed password! Password must include numbers, letters, uppercase and lowercase letters!\n");
             return false;
         }
 
-        boolean secondCondition = value.matches("^[\u0000-\u007F]*$"); // Regular Expression showing the password satisfies the condition B
-        if(!secondCondition) {
+        boolean secondCondition = value.matches(PASSWORD_PATTERN_B); // Regular Expression showing the password satisfies the condition B
+        if (!secondCondition) {
             getState().append("Failed password! Password can only be visible symbols of ASCII!\n");
             return false;
         }
 
-        boolean thirdCondition = !matchString("(?:abcd|dcba|bcde|edcb|cdef|fedc|defg|gfed|efgh|hgfe|fghi|ihgf|ghij|jihg|hijk|kjih|ijkl|lkji|jklm|mlkj|klmn|nmlk|lmno|onml|mnop|ponm|nopq|qpon|opqr|rqpo|pqrs|srqp|qrst|tsrq|rstu|utsr|stuv|vuts|tuvw|wvut|uvwx|xwvu|vwxy|yxwv|wxyz|zyxw|0123|1234|2345|3456|4567|5678|6789)",
+        boolean thirdCondition = !matchString(PASSWORD_PATTERN_C,
                 value); // Regular Expression showing the password satisfies the condition C
-        if(!thirdCondition) {
+        if (!thirdCondition) {
             getState().append("Failed password! The number of letters and numbers in a row must be smaller than 4!\n");
             return false;
         }
 
-        boolean fourthCondition = !matchString("(\\w+)\\1{3,}", value); // Regular Expression showing the password satisfies the condition D
-        if(!fourthCondition) {
+        boolean fourthCondition = !matchString(PASSWORD_PATTERN_D,
+                value); // Regular Expression showing the password satisfies the condition D
+        if (!fourthCondition) {
             getState().append("Failed password! The number of same characters are no more than 3!\n");
             return false;
         }
